843f69047a2a6ca874ba9f748b85fcd08941ad5e
aeg <aeg@amazon.com>
2019-01-19 17:30:09 +1100

    Add multiprocessing

--- source/main.py
+++ source/main.py
@@ -3,13 +3,14 @@ import logging
 import os
 
 import sys
+from queue import Queue
+from threading import Thread
 
 from awsS3Io import AwsS3Io
 from ftp_downloader import FtpDownloader
 
 
 def run(local_path, config_file=None, config_str=None, s3uri=None):
-    logger = logging.getLogger(__name__)
     if config_str is None:
         # If no config str provided, load from file or default
         if config_file is None:
@@ -21,14 +22,35 @@ def run(local_path, config_file=None, config_str=None, s3uri=None):
     config_dict = json.loads(config_str)
 
     downloader = FtpDownloader.load_from_config(config_dict)
+
+    # use thread pool to parallel process
+    q = Queue()
+    t = Thread(target=lambda: worker(q, s3uri))
+    t.start()
     for f in downloader.iterate(local_path=local_path):
-        if s3uri is not None:
-            # Upload to s3 and remove local copy
-            s3io = AwsS3Io()
-            s3io.uploadfile(f, s3uri)
-            # TODO: Use flag to have option to retain local copy
-            logger.info("Uploaded to s3 and deleted local copy..{}".format(f))
-            os.remove(f)
+        q.put(f)
+    # poison pill
+    q.put(None)
+    t.join()
+
+
+def worker(read_queue, s3uri):
+    while True:
+        item = read_queue.get()
+        if item is None:
+            return
+        post_process(item, s3uri)
+
+
+def post_process(f, s3uri):
+    logger = logging.getLogger(__name__)
+    if s3uri is not None:
+        # Upload to s3 and remove local copy
+        s3io = AwsS3Io()
+        s3io.uploadfile(f, s3uri)
+        # TODO: Use flag to have option to retain local copy
+        logger.info("Uploaded to s3 and deleted local copy..{}".format(f))
+        os.remove(f)
 
 
 if __name__ == '__main__':
