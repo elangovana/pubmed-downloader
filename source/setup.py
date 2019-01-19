b8030603b7e355aaa57c93eb2156cc1c3333cc59
aeg <aeg@amazon.com>
2019-01-19 12:32:54 +1100

    Add build files

---
+++ source/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pubmed-downloader',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'source'},
+    url='',
+    license='',
+    author='Aparna Elangovan',
+    author_email='',
+    description=''
+)
